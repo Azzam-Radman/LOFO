2a144f3d98dea215d257da37430223a9e73f2b34
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2022-09-16 22:03:39 +0300

    Update lofo.py

--- lofo.py
+++ lofo.py
@@ -78,7 +78,6 @@ class LOFO:
         all_feats = feats.columns.tolist()
         bad_feats = []
         
-        print(self.is_keras_model)
         print(f'Number of columns before LOFO: {len(all_feats)}')
         print(f'Score before LOFO: {self.cross_validation(feats)}')
         
